17b64041900ffbf24ee341f8666bb2edad987c50
Victor Hugo <vhgouvea@hotmail.com>
2022-04-20 08:59:12 -0300

    removed typeORM

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:12-alpine
 
-WORKDIR /home/api/nestjs
+WORKDIR /home/api/
 
 COPY package.json . 
 COPY package-lock.json .
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './configs/typeorm-config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
